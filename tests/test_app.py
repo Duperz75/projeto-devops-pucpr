f8fa5054c6117c9986444eabc7b89cfd9653ecf3
Duperz75 <imperialedu0075@gmail.com>
2026-04-20 17:17:21 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,16 @@
+from app import soma, multiplicacao
 
+def test_soma_positivos():
+    assert soma(2, 3) == 5
+
+def test_soma_negativos():
+    assert soma(-2, -3) == -5
+
+def test_soma_zero():
+    assert soma(0, 0) == 0
+
+def test_multiplicacao_positivos():
+    assert multiplicacao(2, 3) == 6
+
+def test_multiplicacao_zero():
+    assert multiplicacao(5, 0) == 0
